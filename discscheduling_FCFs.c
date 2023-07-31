8c2488b2c80e6ffe490c4bea7dbf8bedb8a2082f
sanathsshetty <120259170+sanathsshetty@users.noreply.github.com>
2023-07-31 14:23:24 +0530

    Create discscheduling_FCFs.c

---
+++ discscheduling_FCFs.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i,n,req[50],mov=0,cp;
+    printf("enter the current position\n");
+    scanf("%d",&cp);
+    printf("enter the number of requests\n");
+    scanf("%d",&n);
+    printf("enter the request order\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&req[i]);
+    }
+    mov=mov+abs(cp-req[0]);
+    printf("%d->%d",cp,req[0]);
+    for(i=1;i<n;i++){
+    mov=mov+abs(req[i]-req[i-1]);
+    printf("->%d",req[i]);
+}
+printf("\n");
+printf("total head movement=%d\n",mov);
+}
+"""
+enter the current position
+45
+enter the number of requests
+5
+enter the request order
+30
+66
+24
+75
+50
+45->30->66->24->75->50
+total head movement=169
+"""
